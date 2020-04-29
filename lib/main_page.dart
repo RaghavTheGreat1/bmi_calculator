6c807f2b29cb73b7f05cabf4d49bc9f3f08ef060
Raghav Joshi <raghavjoshi079@gmail.com>
2020-04-29 21:50:50 +0530

    added a rough layout

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -8,6 +8,69 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return ;
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                height: 200,
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                height: 200,
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: Container(
+            height: double.infinity,
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: Colors.black,
+            ),
+          ),
+        ),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                height: 200,
+                width: double.infinity,
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
