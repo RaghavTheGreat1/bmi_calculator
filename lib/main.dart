3ef7907988fd1c49904064d1ef0be7b7cbf6d7c2
Raghav Joshi <raghavjoshi079@gmail.com>
2020-04-30 15:01:39 +0530

    Added dark theme and imported Google Fonts package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'main_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+//import 'themes/dark_theme.dart';
+import 'themes/colors.dart';
 
 
 void main() => runApp(MyApp());
@@ -9,6 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
+      theme: ThemeData(
+        primaryColor: kPrimaryDarkThemeColor,
+        scaffoldBackgroundColor: kScaffoldDarkThemeBackgroundColor,
+        textTheme : GoogleFonts.openSansTextTheme(), 
+       ),
+
       home: Scaffold(
         appBar: AppBar(
           title: Text("BMI CALCULATOR"),
--- lib/themes/colors.dart
+++ lib/themes/colors.dart
@@ -1,3 +1,8 @@
 import 'package:flutter/material.dart';
 
-const kMaterialCardColor = Color(0xFF111328);
+
+
+const Color kPrimaryDarkThemeColor = Color(0xFF0A0E21);
+const Color kScaffoldDarkThemeBackgroundColor = Color(0xFF0A0E21);
+
+const Color kMaterialCardColor = Color(0xFF111328);
